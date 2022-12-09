7601b1283abbaddd4588ffe7db386c96ce6e5758
get.dipen@gmail.com <get.dipen@gmail.com>
2022-12-08 21:32:34 -0800

    calculated max1, max2, max3

--- calorie_counting.cpp
+++ calorie_counting.cpp
@@ -7,21 +7,38 @@ int main(int argc, char const *argv[]) {
 
   // read input
   while (!cin.eof()) {
+    // cout << "max1: " << max1 << endl;
+    // cout << "max2: " << max2 << endl;
+    // cout << "max3: " << max3 << endl;
+
     cin >> calories;
-    // cout << "calories: " << calories << endl;
     total_calories += calories;
-    if (max < total_calories)
-      max = total_calories;
 
     cin.get();             // reads in the new line character \n after the input (on the same line)
     char c2 = cin.peek();  // reads first character of the next line
 
-    if (c2 == '\n') {
+    if ((c2 == '\n') || (cin.eof())) {
       // end of this elf
+      // cout << "total calories: " << total_calories << endl;
+
+      if (total_calories > max1) {
+        max3 = max2;
+        max2 = max1;
+        max1 = total_calories;
+      } else if (total_calories > max2) {
+        max3 = max2;
+        max2 = total_calories;
+      } else if (total_calories > max3) {
+        max3 = total_calories;
+      }
+
       total_calories = 0;
     }
   }
 
-  cout << max << endl;
+  cout << "max1: " << max1 << endl;
+  cout << "max2: " << max2 << endl;
+  cout << "max3: " << max3 << endl;
+  cout << "total: " << (max1 + max2 + max3) << endl;
   return 0;
 }
